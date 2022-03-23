770c85685c8df09bcd1d6887c97c12a075aeb46c
Dirk van Zon <dirk.vanzon@theapsgroup.com>
2022-03-23 13:55:22 +0100

    feat: class-validator in Image & Post

--- post-service/src/domain/models/Image.ts
+++ post-service/src/domain/models/Image.ts
@@ -1,6 +1,17 @@
+import {IsInt, Min, Max} from "class-validator"
+
+const MIN_SIZE = 640;
+const MAX_SIZE = 1080;
+
 export class Image {
   id: string;
   url: string;
+  @IsInt()
+  @Min(MIN_SIZE)
+  @Max(MAX_SIZE)
   height: number;
+  @IsInt()
+  @Min(MIN_SIZE)
+  @Max(MAX_SIZE)
   width: number;
 }
--- post-service/src/domain/models/Post.ts
+++ post-service/src/domain/models/Post.ts
@@ -1,9 +1,17 @@
 import { Image } from './Image';
+import { IsDate, IsArray, ValidateNested, ArrayMinSize, ArrayMaxSize, IsString, Length } from 'class-validator';
 
 export class Post {
   id: string;
+  @IsString()
+  @Length(1, 100)
   description: string;
+  @IsDate()
   createdAt: Date;
   createdBy: string;
+  @IsArray()
+  @ValidateNested({ each: true })
+  @ArrayMinSize(1)
+  @ArrayMaxSize(5)
   images: Image[];
 }
