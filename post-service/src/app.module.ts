6683040a9d757af9c761244c328588a656aec2db
Dirk van Zon <dirk.vanzon@theapsgroup.com>
2022-03-23 14:34:26 +0100

    chore: added cqrs module to app.module

--- post-service/src/app.module.ts
+++ post-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { QueryHandlers } from './application/queryHanders';
+import { CqrsModule } from "@nestjs/cqrs";
 
 @Module({
-  imports: [],
+  imports: [CqrsModule],
   controllers: [],
   providers: [...QueryHandlers],
 })
