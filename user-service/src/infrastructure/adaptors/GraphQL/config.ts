27f34b17bf3c8aea87bf1a1e68c8cd62cc532741
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-04-13 11:20:19 +0200

    feat: disable graphql playground in production

--- user-service/src/infrastructure/adaptors/GraphQL/config.ts
+++ user-service/src/infrastructure/adaptors/GraphQL/config.ts
@@ -1,11 +1,13 @@
 import { ApolloDriverConfig, ApolloFederationDriver } from "@nestjs/apollo";
 
+const { NODE_ENV } = process.env;
+
 export function GraphQLConfig(): ApolloDriverConfig {
     return {
         logger: console,
         driver: ApolloFederationDriver,
         debug: false,
-        playground: true,
+        playground: NODE_ENV !== 'prod',
         autoSchemaFile: 'userschema.gql',
     }
 }
