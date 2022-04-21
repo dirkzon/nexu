f806d49fd2b8101d0e36a330364889932aa9978e
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-04-21 10:19:13 +0200

    feat: registering in front end

--- api-gateway/src/dataSourceMethods.ts
+++ api-gateway/src/dataSourceMethods.ts
@@ -3,7 +3,9 @@ import { RemoteGraphQLDataSource } from "@apollo/gateway";
 export class dataSourceMethods extends RemoteGraphQLDataSource {
     willSendRequest({ request, context }) {
         if (!!context.req) {
-            request.http.headers.set('user', context.req.headers.user);
-        }       
+            if(!!context.req.headers.user){
+                request.http.headers.set('user', context.req.headers.user);
+            }
+        }     
     }
 }
--- web-client/src/store/user/actions.ts
+++ web-client/src/store/user/actions.ts
@@ -4,13 +4,18 @@ import axios from "axios";
 
 export const actions: ActionTree<UserState, any> = {
     async Authorize(state, {user, pass}): Promise<any> {
+        const scope = ["get:self", "get:other-users"]
         await axios({
             url: 'http://localhost:5000/graphql',
             method: 'post',
             data: {
                 query: `
                     mutation {
-                        login(user: "${user}", password:"${pass}"){
+                        login(
+                            user: "${user}", 
+                            password:"${pass}",
+                            scope: "${scope}"
+                            ){
                             tokenType,
                             accessToken,
                         }
@@ -21,10 +26,39 @@ export const actions: ActionTree<UserState, any> = {
                 if (result.data.errors) {
                     throw new Error(result.data.errors[0].message);
                 }
-                state.commit("SET_AUTH", result.data.data.login);
+                console.log(result.data.data.login)
+                state.commit("SET_AUTH", result.data.data.login.accessToken);
             }).catch((err) => {
                 console.log(err);
                 throw err;
             });
-    }
+    },
+
+    async Register(state, {user_name, email, password, bio}): Promise<any> {
+        await axios({
+            url: 'http://localhost:5000/graphql',
+            method: 'post',
+            data: {
+                query: `
+                    mutation {
+                        CreateUser(new_user: {name:"${user_name}", password:"${password}", email:"${email}", bio:"${bio}"}){
+                            createdAt,
+                        }
+                    }
+                    `
+            }
+        }).then((result) => {
+            console.log(result);
+            if (result.data.errors) {
+                throw new Error(result.data.errors[0].message);
+            }
+        }).catch((err) => {
+            console.log(err);
+            throw err;
+        });
+    },
+
+    async GetSelf(state): Promise<any> {
+        console.log("yeet")
+    },
 }
