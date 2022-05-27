fdea1375dbe7b94c6c573b7328287a8833656de4
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-05-27 11:55:00 +0200

    feat: comment-service pipeline

--- comment-service/src/main.ts
+++ comment-service/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const { SERVICE } = process.env;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(7000).then(() => console.log(`${SERVICE} started`));
+  await app.listen(7000).then(() => console.log(`comment started`));
 }
 bootstrap();
