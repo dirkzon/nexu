896bcfa7a33bfc480d8bb986245ef52ed21ad1e8
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-06-06 12:56:07 +0200

    feat: added mongodb manifest

--- auth-service/src/infrastructure/adaptors/MongoDB/config.ts
+++ auth-service/src/infrastructure/adaptors/MongoDB/config.ts
@@ -1,19 +1,19 @@
-import { ModelDefinition } from "@nestjs/mongoose";
-import { AuthEntity, AuthSchema } from "./models/auth.schema";
+import { ModelDefinition } from '@nestjs/mongoose';
+import { AuthEntity, AuthSchema } from './models/auth.schema';
 
 const { NODE_ENV } = process.env;
 
 export function MongoOptionsConfig(): string {
-    return NODE_ENV === 'prod' ? 
-        "mongodb://auth-mongo/auth-service" :
-        "mongodb://localhost/auth-service";
+  return NODE_ENV === 'prod'
+    ? 'mongodb://auth-service-database/auth-service'
+    : 'mongodb://localhost/auth-service';
 }
 
 export function MongoFeatureConfig(): ModelDefinition[] {
-    return [
-        {
-            name: AuthEntity.name,
-            schema: AuthSchema,
-        }
-    ]
+  return [
+    {
+      name: AuthEntity.name,
+      schema: AuthSchema,
+    },
+  ];
 }
