b6c217ca20498b301dd6727ada2fba16dbd0109f
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-06-12 14:08:50 +0200

    chore: updated file import

--- auth-service/src/application/ports/AuthStore.ts
+++ auth-service/src/application/ports/AuthStore.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "../../domain/models/User";
+import { Injectable } from '@nestjs/common';
+import { User } from '../../domain/models/user';
 
 @Injectable()
 export abstract class AuthStore {
-    abstract getUserByData(user_data: string): Promise<User>;
-    abstract getUserById(id: string): Promise<User>;
-    abstract createUser(new_user: User): Promise<User>;
-    abstract updateUser(user: User): Promise<User>;
-    abstract deleteUser(id: string): Promise<void>;
+  abstract getUserByData(user_data: string): Promise<User>;
+  abstract getUserById(id: string): Promise<User>;
+  abstract createUser(new_user: User): Promise<User>;
+  abstract updateUser(user: User): Promise<User>;
+  abstract deleteUser(id: string): Promise<void>;
 }
